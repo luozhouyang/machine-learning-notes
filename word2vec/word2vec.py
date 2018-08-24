c8a42b62c13df68c7906c1556c371368b3fb6abb
luozhouyang <stupidme.me.lzy@gmail.com>
2018-08-24 20:39:28 +0800

    Update word2vec

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -12,85 +12,99 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import math
+import os
 
+import numpy as np
 import tensorflow as tf
-from .configs import Configs
 
-VOCAB_SIZE = 100000
+from .data import SkipGramDataSet
+
+dataset = SkipGramDataSet(os.path.join(os.path.curdir, "word2vec/test.txt"))
+
+VOCAB_SIZE = dataset.vocab_size
 EMBEDDING_SIZE = 128
 SKIP_WINDOW = 2
 
 NUM_SAMPLED = 64
 
 BATCH_SIZE = 32
-
+WINDOW_SIZE = 2
 LOG_DIR = "/tmp/word2vec"
 
-TRAIN_STEPS = 100001
+TRAIN_STEPS = 10000
+
+LEARNING_RATE = 0.1
 
 
-class SkipGramModel(object):
+class Word2Vec(object):
 
-  def __init__(self, configs=Configs):
-    self._build_graph()
-
-  def _build_graph(self):
+  def __init__(self):
     self.graph = tf.Graph()
     with self.graph.as_default():
       with tf.name_scope("inputs"):
-        self.train_inputs = tf.placeholder(dtype=tf.int32, shape=[BATCH_SIZE])
-        self.train_labels = tf.placeholder(dtype=tf.int32, shape=[BATCH_SIZE])
+        self.x = tf.placeholder(shape=(None, VOCAB_SIZE), dtype=tf.float32)
+        self.y = tf.placeholder(shape=(None, VOCAB_SIZE), dtype=tf.float32)
 
-      with tf.device("/cpu:0"):
-        with tf.name_scope("embeddings"):
-          self.embeddings = tf.Variable(
-            tf.random_uniform([BATCH_SIZE, EMBEDDING_SIZE], -1.0, 1.0))
-          self.embedded_inputs = tf.nn.embedding_lookup(
-            self.embeddings, self.train_inputs)
+      with tf.name_scope("layer1"):
+        self.W1 = tf.Variable(
+          tf.random_uniform([VOCAB_SIZE, EMBEDDING_SIZE], -1, 1),
+          dtype=tf.float32)
+        self.b1 = tf.Variable(tf.random_normal([EMBEDDING_SIZE]),
+                              dtype=tf.float32)
+      hidden = tf.add(self.b1, tf.matmul(self.x, self.W1))
 
-        with tf.name_scope("weights"):
-          self.nce_weights = tf.Variable(
-            tf.truncated_normal([VOCAB_SIZE, EMBEDDING_SIZE],
-                                stddev=1.0 / math.sqrt(EMBEDDING_SIZE)))
-          with tf.name_scope("bias"):
-            self.nce_bias = tf.Variable(tf.zeros([VOCAB_SIZE]))
+      with tf.name_scope("layer2"):
+        self.W2 = tf.Variable(
+          tf.random_uniform([EMBEDDING_SIZE, VOCAB_SIZE], -1, 1),
+          dtype=tf.float32)
+        self.b2 = tf.Variable(tf.random_normal([VOCAB_SIZE]),
+                              dtype=tf.float32)
 
-      with tf.name_scope("loss"):
-        self.loss = tf.reduce_mean(
-          tf.nn.nce_loss(
-            weights=self.nce_weights,
-            biases=self.nce_bias,
-            labels=self.train_labels,
-            inputs=self.embedded_inputs,
-            num_sampled=NUM_SAMPLED,
-            num_classes=VOCAB_SIZE))
+      self.prediction = tf.nn.softmax(
+        tf.add(tf.matmul(hidden, self.W2), self.b2))
 
-      tf.summary.scalar("loss", self.loss)
+      log = self.y * tf.log(self.prediction)
+      self.loss = tf.reduce_mean(
+        -tf.reduce_sum(log, reduction_indices=[1], keepdims=True))
 
-      with tf.name_scope("optimizer"):
-        self.optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(
-          self.loss)
+      self.opt = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(
+        self.loss)
 
-  def _generate_batch_inputs(self):
-    raise NotImplementedError()
+  def _one_hot_input(self, dataset):
+    features, labels = dataset.gen_batch_inputs(BATCH_SIZE, WINDOW_SIZE)
+    f, l = [], []
+    for w in features:
+      tmp = np.zeros([VOCAB_SIZE])
+      tmp[w] = 1
+      f.append(tmp)
+    for w in labels:
+      tmp = np.zeros(VOCAB_SIZE)
+      tmp[w] = 1
+      l.append(tmp)
+    return f, l
 
-  def train(self):
-    merged_summary = tf.summary.merge_all()
+  def train(self, dataset, n_iters, ):
     with tf.Session(graph=self.graph) as sess:
-      writer = tf.summary.FileWriter(LOG_DIR, graph=sess.graph)
-
       sess.run(tf.global_variables_initializer())
+      for i in range(n_iters):
+        features, labels = self._one_hot_input(dataset)
 
-      avg_loss = 0
-      for step in range(TRAIN_STEPS):
-        inputs, labels = self._generate_batch_inputs()
+        predi, loss = sess.run([self.prediction, self.loss],
+                               feed_dict={
+                                 self.x: features,
+                                 self.y: labels
+                               })
+        print("loss:%s" % loss)
 
-        feed_dict = {self.train_inputs: inputs,
-                     self.train_labels: labels}
+  def predict(self):
+    pass
 
-        _, summary, loss = sess.run([self.optimizer, merged_summary, self.loss],
-                                    feed_dict=feed_dict)
+  def nearest(self, n):
+    pass
 
-        avg_loss += loss
-        writer.add_summary(summary, step)
+  def similarity(self, a, b):
+    pass
+
+
+word2vec = Word2Vec()
+word2vec.train(dataset, TRAIN_STEPS)
